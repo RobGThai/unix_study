3c09fd85f1aa17b2305f2fe4be72bf9599e945ba
Poohdish R <poohdish@hey.com>
2020-10-13 23:35:48 +0700

    Add read terminal.

---
+++ terminal/r.c
@@ -0,0 +1,16 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<errno.h>
+#include<stdlib.h>
+
+int main() {
+    ssize_t nread; 
+    int max = 100;
+    char *s = malloc(100);
+    
+    printf("FD: %i\n", STDIN_FILENO);
+    nread = read(STDIN_FILENO, s, max - 1);
+    printf("Err: %i\n", errno);
+    printf("Read: %s\n Char: %ld\n", s, nread);
+    return 0;
+}
