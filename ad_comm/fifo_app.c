b13574fb80eaf7fb22a557a83f7de53f85377f9b
Poohdish R <poohdish@hey.com>
2021-01-03 01:48:03 +0700

    Fix bug where FIFO shutdown on receiver ends before the msg is sent.

--- ad_comm/fifo_app.c
+++ ad_comm/fifo_app.c
@@ -7,7 +7,7 @@
 #include <ctype.h>
 
 int main(void) {
-    int fd_server, fd_client, i;
+    int fd_server, fd_server_w, fd_client, i;
     ssize_t nread;
     struct simple_message msg;
     char fifo_name[100];
@@ -18,25 +18,35 @@ int main(void) {
     if (mkfifo(SERVER_FIFO_NAME, perm) == -1 && errno != EEXIST)
         printf("ERROR\n");
 
+    printf("Reading from %s\n", SERVER_FIFO_NAME);
     fd_server = open(SERVER_FIFO_NAME, O_RDONLY);
+    fd_server_w = open(SERVER_FIFO_NAME, O_WRONLY);
 
+    printf("Waiting...\n");
     while (true) {
         nread = read(fd_server, &msg, sizeof(msg));
         if(nread == 0) {
+            printf("Cannot read data\n");
             errno = ENETDOWN;
             exit(EXIT_FAILURE);
         }
+
+        printf("Recieve %s\n", msg.sm_data);
 
         for(i = 0; msg.sm_data[i] != '\0'; i++) {
             msg.sm_data[i] = toupper(msg.sm_data[i]);
         }
         make_fifo_name(msg.sm_clientpid, fifo_name, sizeof(fifo_name));
+        printf("Open client to write %s back at %s.\n", msg.sm_data, fifo_name);
         fd_client = open(fifo_name, O_WRONLY);
+        printf("Sending to %d\n", fd_client);
         write(fd_client, &msg, sizeof(msg));
+        printf("Closing %d\n", fd_client);
         close(fd_client);
     }
 
     close(fd_server);
+    close(fd_server_w);
 
     exit(EXIT_SUCCESS);
 }
--- ad_comm/fifo_app_client.c
+++ ad_comm/fifo_app_client.c
@@ -8,7 +8,7 @@
 #include <string.h>
 
 int main(void) {
-    int fd_server, fd_client, i;
+    int fd_server, fd_client = -1, fd_client_w = -1, i;
     ssize_t nread;
     struct simple_message msg;
     char fifo_name[100];
@@ -27,18 +27,31 @@ int main(void) {
     if (mkfifo(SERVER_FIFO_NAME, perm) == -1 && errno != EEXIST)
         printf("ERROR\n");
 
+    printf("Client name: %s connecting to %s\n", fifo_name, SERVER_FIFO_NAME);
     fd_server = open(SERVER_FIFO_NAME, O_WRONLY);
+    printf("Connected\n");
 
     for(i = 0; word[i] != NULL; i++) {
+        printf("Client sending %s\n", word[i]);
         strcpy(msg.sm_data, word[i]);
         write(fd_server, &msg, sizeof(msg));
+        printf("Write success\n");
 
-        if(fd_client == -1)
+        if(fd_client == -1) {
+            printf("Open fd_client at %s\n", fifo_name);
             fd_client = open(fifo_name, O_RDONLY);
+        }
 
+        if(fd_client_w == -1) {
+            printf("Open fd_client_w at %s\n", fifo_name);
+            fd_client_w = open(fifo_name, O_WRONLY);
+        }
+
+        printf("Reading from %d %d\n", fd_client, fd_client_w);
         nread = read(fd_client, &msg, sizeof(msg));
 
         if(nread == 0) {
+            printf("Read error.\n");
             errno = ENETDOWN;
             exit(EXIT_FAILURE);
         }
@@ -46,6 +59,7 @@ int main(void) {
     }
     close(fd_server);
     close(fd_client);
+    close(fd_client_w);
     unlink(fifo_name);
     printf("Client %ld logging out \n", (long)getpid());
 
